4dc0c4ed9c4909485d42850262d3a3d6a77b1668
Kazaz-Or <or.kazaz@dazn.com>
2023-02-27 12:48:15 +0200

    fix mobile view in navbar

--- components/search/contentSearch/index.tsx
+++ components/search/contentSearch/index.tsx
@@ -29,7 +29,7 @@ const ContentSearch = () => {
           autoComplete="off"
           type="text"
           className="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          placeholder="Search for anything"
+          placeholder="Search"
         />
       </div>
       { results.length > 0 &&
