27484ca75a8ac683e8a197d49e72e185b07eb097
Or Kazaz <83350680+Kazaz-Or@users.noreply.github.com>
2023-02-27 15:56:46 +0200

    On build, build and save search index data (#6)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
-import type { GetStaticProps, NextPage } from 'next'
+import type { GetStaticProps, NextPage } from 'next';
 import Link from 'next/link';
 
 import { BlogList } from '../components/blogs';
 import { BaseLayout } from '../components/layouts';
 import { getBlogs } from '@lib/blogs';
+import { saveSearchData } from '@lib/md';
 import { Blog } from '@interfaces/Blog';
+
 
 type Props = {
   blogs: Blog[]
@@ -27,7 +29,9 @@ const Home: NextPage<Props> = ({blogs}) => {
 }
 
 export const getStaticProps: GetStaticProps = () => {
-  const blogs = getBlogs();  
+  const blogs = getBlogs();
+  saveSearchData(blogs);
+
   return {
     props: {blogs}
   }
