ce221fc86fd9237bf52ff74f83c2658c8553b0b5
Or Kazaz <83350680+Kazaz-Or@users.noreply.github.com>
2023-03-06 15:49:39 +0200

    fix mobile view logo, scope page skeleton & some refinements (#17)

---
+++ pages/scope/index.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable react/no-children-prop */
+import { NextPage } from "next";
+import { useEffect, useState } from "react";
+import ReactMarkdown from "react-markdown";
+
+import { PageLayout } from "@components/layouts";
+
+
+const PortfolioPage: NextPage = () => {
+const [content, setContent] = useState("");
+
+useEffect(() => {
+    fetch("aboutme.md")
+      .then((res) => res.text())
+      .then((text) => setContent(text));
+  }, []);
+
+  return (
+    <>
+        <PageLayout pageTitle="// About Me">
+        <div className="w-2/3 m-auto">
+        <article className="prose lg:prose-md">
+        <div className="mb-14">
+        <ReactMarkdown children={content} />
+        </div>
+        </article>
+        </div>
+        </PageLayout>
+    </>
+  )
+}
+
+
+export default PortfolioPage;
