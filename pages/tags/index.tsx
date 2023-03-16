d4e0f462cb8135a078ba9367ad838df6d2963e21
Kazaz-Or <or.kazaz@dazn.com>
2023-03-16 22:10:41 +0200

    fix tags page

--- pages/tags/index.tsx
+++ pages/tags/index.tsx
@@ -1,6 +1,7 @@
 import { GetStaticProps, NextPage } from 'next';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
+import Image from 'next/image';
 
 import { getBlogs } from '@lib/blogs';
 import { PageLayout } from '@components/layouts';
@@ -15,7 +16,7 @@ const TagsPage: NextPage<Props> = ({ tagCounts }) => {
   return (
     <PageLayout pageTitle="// All Tags of Blog Posts">
       <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 flex">
-        <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg flex-1 p-6">
+        <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg flex-1">
           <h2 className="text-2xl font-bold tracking-tight text-gray-900 mb-4">
             <span className="flex items-center">
               All Tag Categories of Blog Posts
@@ -25,10 +26,6 @@ const TagsPage: NextPage<Props> = ({ tagCounts }) => {
             {tagCounts.map(([tag, count]) => (
               <Link key={tag} legacyBehavior href={`tags/${tag}`}>
                 <a className="border border-gray-200 rounded-lg p-4 hover:border-indigo-500 hover:bg-indigo-500 hover:text-white hover:shadow-md transition duration-300 block">
-                  {/* Replace the emoji with an <img> tag to use a small picture */}
-                  <span role="img" aria-label="emoji" className="mr-2">
-                    📚
-                  </span>
                   <span className="text-lg font-medium text-gray-900 hover:text-white">
                     {tag}
                   </span>
@@ -38,6 +35,17 @@ const TagsPage: NextPage<Props> = ({ tagCounts }) => {
                 </a>
               </Link>
             ))}
+          </div>
+        </div>
+        <div className="border-l border-gray-200 mx-8 h-full hidden sm:block" />
+        <div className="ml-8 w-1/3 hidden md:block">
+          <div className="relative w-full h-auto aspect-w-3 aspect-h-2">
+            <Image
+              src="https://images.unsplash.com/photo-1614741118887-7a4ee193a5fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+              alt=""
+              width={300}
+              height={300}
+            />
           </div>
         </div>
       </div>
