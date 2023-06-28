46bd3313e0fe994c1c3671701da59e8a12185747
Kazaz-Or <or.kazaz@dazn.com>
2023-06-28 17:10:50 +0300

    Add option to not display blog description

--- components/blogs/RelatedBlogs/index.tsx
+++ components/blogs/RelatedBlogs/index.tsx
@@ -22,7 +22,7 @@ const RelatedBlogs: React.FC<{blogs: Blog[]}> = ({ blogs }) => {
         </svg>
       </button>
       <div className="flex-grow">
-        <BlogList blogs={blogs.slice(startIndex, startIndex + 4)} />
+      <BlogList blogs={blogs.slice(startIndex, startIndex + 4)} showDescription={false} />
       </div>
       <button onClick={() => handleScroll('next')} className="self-center">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
--- components/blogs/blogList/index.tsx
+++ components/blogs/blogList/index.tsx
@@ -4,21 +4,22 @@ import { BlogItem } from './BlogItem';
 import { Blog } from '@interfaces/Blog';
 
 type Props = {
-  blogs: Blog[]
-}
+  blogs: Blog[];
+  showDescription?: boolean;
+};
 
-
-const BlogList: FunctionComponent<Props> = ({blogs}) => {
+const BlogList: FunctionComponent<Props> = ({blogs, showDescription = true}) => {
     return (
         <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
         {blogs.map((blog) => (
         <BlogItem
         key={blog.slug}
-        blog={blog}/>
+        blog={blog}
+        showDescription={showDescription}
+        />
         ))}
       </div>
     )
 }
-
 
 export default BlogList;
