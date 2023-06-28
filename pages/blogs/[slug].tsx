d6b7cd539ab0739b700e5062ea7657c87cb36251
Kazaz-Or <or.kazaz@dazn.com>
2023-06-28 16:53:47 +0300

    fix svg attributes classes

--- pages/blogs/[slug].tsx
+++ pages/blogs/[slug].tsx
@@ -55,7 +55,7 @@ const copyToClipboard = () => {
     </a>
   </Link>
     <button onClick={copyToClipboard} className="relative flex items-center" title="Copy to clipboard">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="w-6 h-6">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-6 h-6">
         <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
         <path d="M5 15H4a2 2 0 01-2-2V4a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1"></path>
       </svg>
